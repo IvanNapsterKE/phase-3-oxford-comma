f271c71fd22120fad9d37b08e6a5a8974f2de043
IvanNapsterKE <nguyoivan@gmail.com>
2022-10-24 13:15:51 +0300

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+    if array.count > 2 then
+        array[-1] = "and #{array.last}";
+        array.join(", ")
+    elsif array.count > 1 then
+        array.join(" and ")
+    else
+        array.join()
+    end
+  
 end
+
+# tests
+# puts (oxford_comma(["kiwi", "durian"]))
+# puts (oxford_comma(["kiwi"]))
+# puts (oxford_comma(["kiwi", "durian", "starfruit"]))
